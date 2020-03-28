aff36bac7aa768e99c447239b05b3df2c52786f7
entropyfeng <1092758202@qq.com>
2020-03-28 15:38:48 +0800

    useless update

--- src/main/java/com/github/entropyfeng/mydb/client/TurtleClient.java
+++ src/main/java/com/github/entropyfeng/mydb/client/TurtleClient.java
@@ -2,25 +2,13 @@ package com.github.entropyfeng.mydb.client;
 
 import com.github.entropyfeng.mydb.common.CommonConstant;
 import com.github.entropyfeng.mydb.common.protobuf.TurtleProtoBuf;
-import com.github.entropyfeng.mydb.config.SupportModel;
-import com.github.entropyfeng.mydb.config.SupportObject;
-import com.github.entropyfeng.mydb.core.list.Hello;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.channel.socket.oio.OioSocketChannel;
-import io.netty.handler.codec.protobuf.ProtobufDecoderNano;
-import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.math.BigInteger;
 
 /**
  * @author entropyfeng
@@ -65,15 +53,14 @@ public class TurtleClient {
             .handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
-                   ch.pipeline().addLast(new CommandToByteEncoder());
+                   ch.pipeline().addLast(new TurtleProtoEncoder());
+
                 }
             });
-            
+
             ClientCommandBuilder clientCommandBuilder=new ClientCommandBuilder(TurtleProtoBuf.TurtleModel.ADMIN, TurtleProtoBuf.TurtleObject.VALUE, CommonConstant.HELLO_SERVER);
-
-
             ChannelFuture channelFuture1 = client.connect().sync();
-            logger.info("{} start and bind on {} and connect to {}", this.getClass().getName(), channelFuture.channel().localAddress(), channelFuture.channel().remoteAddress());
+            logger.info("{} start and bind on {} and connect to {}", this.getClass().getName(),channelFuture1.channel().localAddress() ,channelFuture1.channel().remoteAddress());
             channelFuture1.channel().closeFuture().sync();
         } finally {
             eventLoopGroup.shutdownGracefully().sync();
--- src/main/java/com/github/entropyfeng/mydb/client/TurtleProtoEncoder.java
+++ src/main/java/com/github/entropyfeng/mydb/client/TurtleProtoEncoder.java
@@ -4,20 +4,63 @@ import com.github.entropyfeng.mydb.common.protobuf.TurtleProtoBuf;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author entropyfeng
  */
 public class TurtleProtoEncoder extends MessageToByteEncoder<TurtleProtoBuf.ClientCommand> {
-    private ProtobufEncoder protobufEncoder=new ProtobufEncoder();
+
+    private static final Logger logger= LoggerFactory.getLogger(TurtleProtoEncoder.class);
+
 
     @Override
     protected void encode(ChannelHandlerContext ctx, TurtleProtoBuf.ClientCommand msg, ByteBuf out) throws Exception {
-
-        System.out.println("encoding           --");
+        int bodyLen = msg.getSerializedSize();
+        int headerLen = computeRawVarint32Size(bodyLen);
+        out.ensureWritable(headerLen + bodyLen);
+        writeRawVarint32(out, bodyLen);
+        out.writeBytes(msg.toByteArray());
 
     }
 
+
+    /**
+     * Writes protobuf varint32 to (@link ByteBuf).
+     * @param out to be written to
+     * @param value to be written
+     */
+    static void writeRawVarint32(ByteBuf out, int value) {
+        while (true) {
+            if ((value & ~0x7F) == 0) {
+                out.writeByte(value);
+                return;
+            } else {
+                out.writeByte((value & 0x7F) | 0x80);
+                value >>>= 7;
+            }
+        }
+    }
+
+    /**
+     * Computes size of protobuf varint32 after encoding.
+     * @param value which is to be encoded.
+     * @return size of value encoded as protobuf varint32.
+     */
+    static int computeRawVarint32Size(final int value) {
+        if ((value & (0xffffffff <<  7)) == 0) {
+            return 1;
+        }
+        if ((value & (0xffffffff << 14)) == 0) {
+            return 2;
+        }
+        if ((value & (0xffffffff << 21)) == 0) {
+            return 3;
+        }
+        if ((value & (0xffffffff << 28)) == 0) {
+            return 4;
+        }
+        return 5;
+    }
 }
