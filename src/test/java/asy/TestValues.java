7e9b01b9c7ad85bc454565e72557898b292abff8
entropyfeng <1092758202@qq.com>
2020-05-30 14:45:23 +0800

    Update TestValues.java

--- src/test/java/asy/TestValues.java
+++ src/test/java/asy/TestValues.java
@@ -1,20 +1,22 @@
 package asy;
 
 import com.github.entropyfeng.mydb.client.asy.res.AsyValueOperations;
+import com.github.entropyfeng.mydb.client.ops.TurtleTemplate;
 import com.github.entropyfeng.mydb.common.Pair;
 import com.github.entropyfeng.mydb.common.TurtleValue;
 import com.github.entropyfeng.mydb.common.protobuf.ProtoBuf.DataBody;
 import com.github.entropyfeng.mydb.common.protobuf.ProtoBuf.ResHead;
+import com.github.entropyfeng.mydb.server.persistence.AcceptTransThreadFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 
 public class TestValues {
 
+    ExecutorService service = new ThreadPoolExecutor(4, 8, 10, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new AcceptTransThreadFactory());
 
     @Test
     public void testSet() throws Exception {
@@ -33,4 +35,25 @@ public class TestValues {
         Assert.assertTrue(arrayList.get(limit-1).get().getKey().getSuccess());
     }
 
+
+    @Test
+    public void testMulti(){
+
+        int clientNumber = 8;
+        for (int i = 0; i < clientNumber; i++) {
+            service.submit(() -> {
+                try {
+                    testSet();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        service.shutdown();
+        try {
+            service.awaitTermination(10000,TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
