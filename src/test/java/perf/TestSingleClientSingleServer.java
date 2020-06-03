225e5e42320e257ee0765b7c030992b552169b89
entropyfeng <1092758202@qq.com>
2020-06-03 16:20:46 +0800

    Update TestSingleClientSingleServer.java

--- src/test/java/perf/TestSingleClientSingleServer.java
+++ src/test/java/perf/TestSingleClientSingleServer.java
@@ -3,12 +3,14 @@ package perf;
 import com.github.entropyfeng.mydb.client.asy.AsyTemplate;
 import com.github.entropyfeng.mydb.common.Pair;
 import com.github.entropyfeng.mydb.common.TurtleValue;
+import com.github.entropyfeng.mydb.common.protobuf.ProtoBuf;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.*;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import static com.github.entropyfeng.mydb.common.protobuf.ProtoBuf.DataBody;
@@ -20,7 +22,10 @@ import static com.github.entropyfeng.mydb.common.protobuf.ProtoBuf.ResHead;
 public class TestSingleClientSingleServer {
 
     AsyTemplate template = new AsyTemplate("0.0.0.0", 4407);
-    ThreadPoolExecutor executor=new ThreadPoolExecutor(5,5,10, TimeUnit.SECONDS,new ArrayBlockingQueue<>(5));
+
+    int limit = 300000;
+    CountDownLatch countDownLatch=new CountDownLatch(limit*5);
+    ScheduledExecutorService scheduledExecutorService=Executors.newScheduledThreadPool(1);
     @Test
     public void test() {
 
@@ -46,66 +51,30 @@ public class TestSingleClientSingleServer {
     }
 
     @Test
-    public void testMulti(){
-        int limit = 200000;
-        long begin = System.currentTimeMillis();
-        ArrayList<CompletableFuture<Pair<ResHead, Collection<DataBody>>>>  valueList = new ArrayList<>(limit);
-        ArrayList<CompletableFuture<Pair<ResHead, Collection<DataBody>>>> listList = new ArrayList<>(limit);
-        ArrayList<CompletableFuture<Pair<ResHead, Collection<DataBody>>>>  setList = new ArrayList<>(limit);
-        ArrayList<CompletableFuture<Pair<ResHead, Collection<DataBody>>>>  hashList = new ArrayList<>(limit);
-        ArrayList<CompletableFuture<Pair<ResHead, Collection<DataBody>>>>  orderSetList = new ArrayList<>(limit);
+    public  void testMulti(){
 
+        scheduledExecutorService.scheduleAtFixedRate(()-> System.out.println("countdownNumber-> "+countDownLatch.getCount()),5,5,TimeUnit.SECONDS);
+        BiConsumer<Pair<ResHead,Collection<DataBody>>,Throwable> biConsumer=(pair, throwable) -> countDownLatch.countDown();
+
+        long begin = System.currentTimeMillis();
 
         TurtleValue value=new TurtleValue(0);
         for (int i = 0; i < limit; i++) {
-
-            valueList.add(template.opsForValues().get(""));
-            listList.add(template.opsForList().exist(""));
-            setList.add(template.opsForSet().size());
-            hashList.add(template.opsForHash().get("",value));
-            orderSetList.add(template.opsForOrderSet().exists("",value,0d));
+            template.opsForValues().get("").whenComplete(biConsumer);
+            template.opsForList().exist("").whenComplete(biConsumer);
+            template.opsForSet().size().whenComplete(biConsumer);
+            template.opsForHash().get("",value).whenComplete(biConsumer);
+            template.opsForOrderSet().exists("",value,0d).whenComplete(biConsumer);
         }
-        long end = System.currentTimeMillis();
-        System.out.println((end - begin) / 1000);
 
-        Consumer<CompletableFuture<Pair<ResHead, Collection<DataBody>>>> consumer= pairCompletableFuture -> {
-            try {
-                Assert.assertTrue(pairCompletableFuture.get().getKey().getSuccess());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        };
-
-        executor.execute(()->{
-            valueList.forEach(consumer);
-
-            System.out.println("valueEnd");
-        });
-        executor.execute(()->{
-            listList.forEach(consumer);
-            System.out.println("listEnd");
-        });
-        executor.execute(()->{
-            setList.forEach(consumer);
-            System.out.println("settEnd");
-        });
-        executor.execute(()->{
-            orderSetList.forEach(consumer);
-            System.out.println("oSetEnd");
-        });
-        executor.execute(()->{
-            hashList.forEach(consumer);
-            System.out.println("hashEnd");
-        });
-
-        long fin=System.currentTimeMillis();
-        executor.shutdown();
         try {
-            executor.awaitTermination(60,TimeUnit.SECONDS);
+            countDownLatch.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println((fin-begin)/1000);
+        long fin=System.currentTimeMillis();
+
+        System.out.println((fin-begin));
     }
 
 }
